04a295204ffc3d068c5a70666226072931d9b9eb
Unknown <gregory.willcox@gmail.com>
2017-09-26 18:05:12 -0400

    update

--- gulpfile.js/task-config.js
+++ gulpfile.js/task-config.js
@@ -1,17 +1,19 @@
 module.exports = {
   html        : true,
+  javascripts : true,
   images      : true,
   fonts       : true,
   static      : true,
   svgSprite   : true,
   ghPages     : true,
   stylesheets : true,
+  subscribe   : true,
 
   javascripts: {
     entry: {
       // files paths are relative to
       // javascripts.dest in path-config.json
-      app: ["./app.js"]
+      app: ["./gw_countdown.js"]
     }
   },
 
