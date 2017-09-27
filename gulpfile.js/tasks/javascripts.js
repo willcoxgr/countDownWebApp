f9934e56cc1a648961b6de6f75df6699b489a54f
Unknown <gregory.willcox@gmail.com>
2017-09-27 15:39:32 -0400

    updated gulpfile.js - modified javascript.js and added subscribe.js

--- gulpfile.js/tasks/javascripts.js
+++ gulpfile.js/tasks/javascripts.js
@@ -1,24 +1,21 @@
 if(!TASK_CONFIG.javascripts) return
 
-const changed = require('gulp-changed')
-const gulp    = require('gulp')
-const path    = require('path')
+var browserSync = require('browser-sync')
+var changed     = require('gulp-changed')
+var gulp        = require('gulp')
+var path        = require('path')
 
-const javascriptsTask = function() {
-  const srcPath = path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.javascripts.src)
-  const defaultSrcOptions = { dot: true }
-  const options = Object.assign(defaultSrcOptions, (TASK_CONFIG.javascripts.srcOptions || {}))
+var javascriptsTask = function() {
 
-  const paths = {
-    src: [
-      path.join(srcPath, '**/*'),
-      path.resolve(process.env.PWD, '!' + PATH_CONFIG.src, PATH_CONFIG.javascripts.src, 'README.md')
-    ],
+  var paths = {
+    src: path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.javascripts.src, '**/*.{' + TASK_CONFIG.javascripts.extensions + '}'),
     dest: path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.javascripts.dest)
   }
 
-  return gulp.src(paths.src, options)
+  return gulp.src([paths.src, , '*!README.md'])
+    .pipe(changed(paths.dest)) // Ignore unchanged files
     .pipe(gulp.dest(paths.dest))
+    .pipe(browserSync.stream())
 }
 
 gulp.task('javascripts', javascriptsTask)
